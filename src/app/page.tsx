344a2460795e1a86a5fdea5f6a849163c701d090
Manas Laud <119937760+manaslaud@users.noreply.github.com>
2023-08-09 01:09:52 +0530

    small change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,10 @@ import { useEffect, useRef } from 'react'
 import { Overlay } from './components/Overlay'
 import { ScrollControls,Scroll } from '@react-three/drei'
 export default function Home() {
+  const isMobile= window.innerWidth >=320 && window.innerWidth<425
   return (
     <div className='w-screen h-screen'>
-    <Canvas camera={{fov:58}} className='w-6/12 h-6/12'>
+    <Canvas camera={{fov:(isMobile?100:58)}} className='w-6/12 h-6/12'>
     
       <ambientLight/>
       <rectAreaLight position={[0,0,5]} color="red" width={10} height={10}/>
