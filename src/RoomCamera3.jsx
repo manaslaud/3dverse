eb2c31cd510c73197a1357f3b62d6d29c8908324
Manas Laud <119937760+manaslaud@users.noreply.github.com>
2023-08-10 21:37:55 +0530

    responsivity:phone/laptop

--- src/RoomCamera3.jsx
+++ src/RoomCamera3.jsx
@@ -6,6 +6,18 @@ import { Vector2 } from 'three/src/Three';
 import gsap from 'gsap';
 
 export default function Model(props) {
+  const phoneScaleFactor=0.5
+  let scaleFactor=1;
+  console.log(props.props)
+  if(props.props==1){
+    scaleFactor=phoneScaleFactor * window.innerWidth/345
+  }
+  if(props.props==2){
+    scaleFactor=0.9
+  }
+  if(props.props==3){
+    scaleFactor=1
+  }
   const { scene,camera } = useThree();
   const { nodes,materials ,cameras } = useGLTF('/rooms/roomCamera3.glb');
   cameras[0].position.set(0, 0, 3);
@@ -65,7 +77,7 @@ export default function Model(props) {
 
   return (
     <group {...props} dispose={null}>
-    <group position={[(props.props<0.5)?1.3-props.props:2.6-props.props, -12.0, 0]} rotation={[Math.PI, 0, Math.PI]} scale={0.01*props.props}>
+    <group position={[props.props>=2?1.45*window.innerWidth/1300:-0.1, props.props>=2?-13:-10.6, 0]} rotation={[Math.PI, 0, Math.PI]} scale={0.01*scaleFactor}>
       <group position={[65, 0, 40]} rotation={[-Math.PI, 0, -Math.PI]}>
         <group scale={[1, 1, 1.5]}>
           <group scale={[1, 1, 0.667]}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,34 @@
 "use client"
 import { Canvas, useFrame } from '@react-three/fiber'
 import CustomOrbitControls from './components/OrbitControls'
-import RoomsContext from './components/Rooms'
-import { useEffect, useRef } from 'react'
-import { Overlay } from './components/Overlay'
 import { ScrollControls,Scroll } from '@react-three/drei'
+import RoomCamera1 from '../RoomCamera1'
+import { Overlay } from './components/Overlay'
+import RoomCamera2 from '../RoomCamera2'
+import RoomCamera3 from '../RoomCamera3'
+import { useEffect } from 'react'
 export default function Home() {
+  const isMobile= window.innerWidth>=320 && window.innerWidth<728
+  const isTab= window.innerWidth>=728 && window.innerWidth<1024
+  const isLaptop= window.innerWidth>=1024
+  let sw=isMobile ? 1 : (isTab ? 2 : 3)
+  
+  
   return (
     <div className='w-screen h-screen'>
-    <Canvas camera={{fov:58}} className='w-6/12 h-6/12'>
-    
+    <Canvas camera={{fov:58}} >
+    <ScrollControls pages={isMobile ? 3.2 : 3.1} >
+        <Scroll>
+          <RoomCamera1 props={sw}/>
+          <RoomCamera2 props={sw}/>
+          <RoomCamera3 props={sw}/>
+        </Scroll>
+        <Overlay/>
+        </ScrollControls>
       <ambientLight/>
       <rectAreaLight position={[0,0,5]} color="red" width={10} height={10}/>
-      <RoomsContext/>
       <CustomOrbitControls/>
-      <directionalLight color="white" intensity={1} position={[0,0,5]}/> 
-    </Canvas>
+      </Canvas>
     </div>
   )
 }
